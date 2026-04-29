7067039afb293e6c77501e27bc78e7b1fa61aee7
Xavi <187180531+bcxavi@users.noreply.github.com>
2026-04-29 18:34:49 +0200

    Add tempConversion solution
    Convert temperatures between Celsius and Fahrenheit with rounded results

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const round = num => Math.round(num * 10) / 10;
+
+const convertToCelsius = function(temp) {
+  return round((temp - 32) * 5 / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return round((temp * 9 / 5) + 32);
 };
 
 // Do not edit below this line
