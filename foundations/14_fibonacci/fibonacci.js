d735accba5f66d3541e50b7085cae8d6fcc86a10
Xavi <187180531+bcxavi@users.noreply.github.com>
2026-05-02 11:27:12 +0200

    Improve fibonacci function
    Handle string input, negative numbers, and base cases for sequence calculation

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
 
+    if (num < 0) return "OOPS";
+
+    let fib = [0, 1];
+
+    for (let i = 2; i <= num; i++) {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
+
+    return fib[num];
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
